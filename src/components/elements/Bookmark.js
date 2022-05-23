0201bc972848bc7132e2954de55865e75cae54fe
chocoboleda <marioapolinar98@gmail.com>
2022-05-22 21:36:12 -0500

    Lógica de favoritos cambiada

--- src/components/elements/Bookmark.js
+++ src/components/elements/Bookmark.js
@@ -4,9 +4,9 @@ import DeleteIcon from '@mui/icons-material/Delete';
 export default function BookMark({element,onDelete}){
     return(
         <div className="bookmark">
-            <img className="bookmark-photo-div" src={element.image} alt="image"/>
+            <img className="bookmark-photo-div" src={element.image} alt={element.image}/>
             <span>{element.name}</span>
-            <DeleteIcon className="delete-icon" onClick={() => onDelete(element)}/>
+            <DeleteIcon className="delete-icon" onClick={() => onDelete(element)} key={element.id}/>
         </div>
     )
 }
